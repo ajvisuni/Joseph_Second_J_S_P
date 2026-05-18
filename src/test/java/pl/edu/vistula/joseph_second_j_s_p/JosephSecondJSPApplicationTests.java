17b87393cc0dfd7286b0362b465e72ed224c599f
ajvisuni <a.joseph.adv.corp@gmail.com>
2026-05-18 06:27:45 +0200

    Inital Commit

---
+++ src/test/java/pl/edu/vistula/joseph_second_j_s_p/JosephSecondJSPApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.joseph_second_j_s_p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosephSecondJSPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
